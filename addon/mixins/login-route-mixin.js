341d64c997ab078a0faf629b27f2e823368d56c2
Qi He <qhe@heyook.com>
2015-11-18 10:46:38 -0800

    update login route mixin

--- addon/mixins/login-route-mixin.js
+++ addon/mixins/login-route-mixin.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Mixin.create({
+
+  session: service('session'),
 
   beforeModel: function() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('/');
+    } else {
+      return this._super(...arguments);
     }
   },
 
